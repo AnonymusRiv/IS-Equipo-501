4ffb45a85fb2784b18e66f7fb869828803511f48
Albapj01 <83663505+Albapj01@users.noreply.github.com>
2021-12-01 17:51:29 +0100

    Upload Maquina

--- maquina.cc
+++ maquina.cc
@@ -1,1 +1,30 @@
 #include "maquina.h"
+
+using namespace std;
+
+Maquina::Maquina(int machine_ID, int machine_Nucleus, int machine_NOcuped)
+{
+	M_ID_ = machine_ID;
+	M_Nucleus_ = machine_Nucleus;
+	M_NOcuped_ = machine_NOcuped;
+}
+
+bool setNucleus(int machine_Nucleus)
+{
+	if(machine_Nucleus.empty())
+	{
+		return false;
+	}
+	M_Nucleus_ = machine_Nucleus;
+	return true;
+}
+
+bool setNOcuped(int machine_NOcuped, int OTime)
+{
+
+}
+
+bool setDate(date machine_Date)
+{
+
+}
