1ec947c0eecf48b339ed349a133ca3bb8ab814d1
Silvia Roldán <83606732+Silvya292@users.noreply.github.com>
2021-12-07 21:03:10 +0100

    Definición de la clase Reserva actualizada

---
+++ reserva.h
@@ -0,0 +1,35 @@
+#ifndef RESERVA_H
+#define RESERVA_H
+
+#include <string>
+#include <ctime>
+#include <list>
+#include "Usuario_Normal.h"
+#include "Usuario_Administrador.h"
+
+typedef struct{
+    string day;
+    string month;
+    string year;
+}date;
+
+class Reserva{
+    private:
+        int R_ID_;
+        int R_User_;
+        date R_Date_;
+        int R_Machine;
+        int R_Nucleus;
+        int R_Time;
+    public:
+        Reserva(int res_ID=0, int user_ID, date res_date, int res_Machine, int res_Nucleus, int res_Time);
+        bool setDate(date date);
+        bool setTime(int time);
+        bool setMachine(int machine_ID);
+        bool setNucleus(int nucleus);
+        list <string> listReservas(int user_ID);
+        string modificarReserva(int user_ID, int reserva_ID);
+        bool deleteReserva(int user_ID, int reserva_ID);
+};
+
+#endif
