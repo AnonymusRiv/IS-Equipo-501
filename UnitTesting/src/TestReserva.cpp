788ffdac360707d44ec1e30bf87e1342cb18fd2a
Silvia Roldán <silviarf2902@gmail.com>
2021-12-10 19:58:36 +0100

    feat: Test de la clase Reserva actualizados

--- UnitTesting/src/TestReserva.cpp
+++ UnitTesting/src/TestReserva.cpp
@@ -29,10 +29,58 @@ void testSetDate(){
 	ASSERT(resultado==true);
 }
 
+void testSetTime(){
+	date hoy;
+	Reserva res1=Reserva(1,hoy,1,1,-2);
+	bool resultado=res1.setTime(-2);
+	ASSERT(resultado==false);
+
+	Reserva res2=Reserva(1,hoy,1,1,2);
+	bool resultado=res2.setTime(2);
+	ASSERT(resultado==true);
+}
+
+void testSetMachine(){
+	date hoy;
+	Reserva res1=Reserva(1,hoy,51,1,1);
+	bool resultado=res1.setMachine(51);
+	ASSERT(resultado==false);
+
+	Reserva res2=Reserva(1,hoy,1,1,1);
+	bool resultado=res2.setMachine(1);
+	ASSERT(resultado==true);
+}
+
+void testSetNucleus(){
+	date hoy;
+	Reserva res1=Reserva(1,hoy,1,15,1);
+	bool resultado=res1.setNucleus(15);
+	ASSERT(resultado==false);
+
+	Reserva res2=Reserva(1,hoy,1,3,1);
+	bool resultado=res2.setNucleus(3);
+	ASSERT(resultado==true);
+}
+
+void testDeleteReserva(){
+	date hoy;
+	Reserva res1=Reserva(-2,hoy,1,1,1,2);
+	bool resultado=res1.deleteReserva(-2,2);
+	ASSERT(resultado==false);
+
+	date hoy;
+	Reserva res2=Reserva(2,hoy,1,1,1,2);
+	bool resultado=res1.deleteReserva(2,2);
+	ASSERT(resultado==true);
+}
+
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	s.push_back(CUTE(testSetDate));
-	
+	s.push_back(CUTE(testSetTime));
+	s.push_back(CUTE(testSetMachine));
+	s.push_back(CUTE(testSetNucleus));
+	s.push_back(CUTE(testDeleteReserva));
 	s.push_back(CUTE(thisIsATest));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
