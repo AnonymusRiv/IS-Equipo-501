eeeb27460bfe4f96d8a57788f2a8dc2fc274d678
Albapj01 <83663505+Albapj01@users.noreply.github.com>
2021-12-11 19:50:38 +0100

    Creación de test de la clase Máquina

---
+++ Unittesting_Maquina/Debug/src/Test.d
@@ -0,0 +1,82 @@
+src/Test.o: ../src/Test.cpp \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_base.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_to_string.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_demangle.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_version.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_integer_sequence.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_equals.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_diff_values.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_traits.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_deprecated.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_range.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_relops.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_data_driven.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_throws.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_suite.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_test.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_library.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_repeated_test.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_suite_test.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_test_incarnate.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_testmember.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_version.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/ide_listener.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_listener.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/xml_listener.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_xml_file.h \
+  /Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_runner.h
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_base.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_to_string.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_demangle.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_version.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_integer_sequence.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_equals.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_diff_values.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_traits.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_deprecated.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_range.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_relops.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_data_driven.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_throws.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_suite.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_test.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_determine_library.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_repeated_test.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_suite_test.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_test_incarnate.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_testmember.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_version.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/ide_listener.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_listener.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/xml_listener.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_xml_file.h:
+
+/Users/albapalomino/eclipse/cpp-2021-09/Eclipse.app/Contents/Eclipse/unittesting/cute/cute_runner.h:
