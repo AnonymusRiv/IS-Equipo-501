8dbd6702e564cd3ddf16f71f6655f743941d442b
Albapj01 <83663505+Albapj01@users.noreply.github.com>
2021-12-14 14:24:26 +0100

    Corrección de errores de la clase máquina

--- maquina.cc
+++ maquina.cc
@@ -23,17 +23,16 @@ bool Maquina::setNucleus(int machine_Nucleus)
 		M_Nucleus_ = machine_Nucleus;
 		return true;
 	}
-	cout<<"Debe introducir un numero positivo\n"<<endl;
+	cout<<"Debe introducir un numero positivo"<<endl;
 	return false;
-	
 }
 
 bool Maquina::setID(int machine_ID)
 {
-	ifstream f("machine.txt");
+	ifstream f("Maquinas.txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
+		cout<<"Error al abrir el fichero"<<endl;
 		EXIT_FAILURE;
 	}
 
@@ -59,8 +58,8 @@ bool Maquina::findMachine(int machine_ID)
 	ifstream f(machine_ID+".txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
-		EXIT_FAILURE;
+		cout<<"Error al abrir el fichero"<<endl;
+		return false;
 	}
 
 	int election;
@@ -70,10 +69,12 @@ bool Maquina::findMachine(int machine_ID)
 		if(maquina == to_string(machine_ID))
 		{
 			f.close();
+			M_ID_=machine_ID;
 			return true;
 		}
 	}
 	f.close();
+    cout << "ERROR. No existe en el sistema el identificador de la máquina indicada"<<endl;
 	return false;
 }
 
@@ -82,63 +83,109 @@ bool Maquina::selectMachine(int machine_ID, date date, int time, int nucleus)
 	ifstream f(machine_ID+".txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
-		EXIT_FAILURE;
+		cout<<"Error al abrir el fichero"<<endl;
+		return false;
 	}
 
 	int election;
 	string maquina;
-	while(!f.eof())
-	{
-		if(maquina == to_string(machine_ID))
-		{
-			if(nucleus<=(M_Nucleus_ - M_NOcuped_)
-			{
-				f.close();
-				return true;
-			}
-		}
-	}
-	f.close();
-	return false;
-}
 
-list <string> Maquina::listMachine(date date, int time, int nucleus)
-{
-	ifstream f("machine.txt");
-	if(!f)
-	{
-		cout<<"Error al abrir el fichero\n"<<endl;
-		EXIT_FAILURE;
-	}
-
+	int nucle;
+	int nucleusOcup;
 
 	string line;
 	string nucl;
 	string nocup;
+	string m_id;
 
-	string delimiter=' ';
+	string delimiter=" ";
 	getline(f, line, '\n');
+	line.erase(0, line.find(delimiter) + delimiter.length());
+	m_id = line.substr(0, line.find(delimiter));
 	line.erase(0, line.find(delimiter) + delimiter.length());
 	nucl = line.substr(0, line.find(delimiter));
 	line.erase(0, line.find(delimiter) + delimiter.length());
 	nocup = line.substr(0, line.find(delimiter));
 
-
-
-	while(!f.eof)
+	nucle=stoi(nucl);
+	nucleusOcup=stoi(nocup);
+	while(!f.eof())
 	{
-		list <string> n;
-
-		if(nucleus<=(nucl - nocup)
+		if(maquina == to_string(machine_ID))
 		{
+			if(nucleus<=(nucle - nucleusOcup))
+			{
+				f.close();
+				fstream f1(to_string(machine_ID)+".txt");
+				if(!f1)
+				{
+					cout<<"Error al abrir el fichero"<<endl;
+					return false;
+				}
 
-			f.close();
-			n.push_back;
-			return n;
+				int cont=1;
+				string info;
+				getline(f1, info, '\n');
+				while(!f1.eof())
+				{
+					cont++;
+					getline(f1, info, '\n');
+				}
+				M_ID_ = cont+1;
+				nucleus = nucle+nucleusOcup;
+
+				f1 << M_ID_ << " " << nucleus << " " << nucleusOcup << endl;
+
+				f1.close();
+				return true;
+			}
 		}
 	}
 	f.close();
+	cout << "ERROR. No existe en el sistema el identificador de la máquina indicada"<<endl;
+	return false;
+}
+
+list <string> Maquina::listMachine(date date, int time, int nucleus)
+{
+	ifstream f("Maquinas.txt");
+	if(!f)
+	{
+		cout<<"Error al abrir el fichero"<<endl;
+		EXIT_FAILURE;
+	}
+
+	string line;
+	string nucl;
+	string nocup;
+	string m_id;
+
+	list <string> n;
+
+	int nucle;
+	int nucleusOcup;
+
+	string delimiter=" ";
+	getline(f, line, '\n');
+	line.erase(0, line.find(delimiter) + delimiter.length());
+	m_id = line.substr(0, line.find(delimiter));
+	line.erase(0, line.find(delimiter) + delimiter.length());
+	nucl = line.substr(0, line.find(delimiter));
+	line.erase(0, line.find(delimiter) + delimiter.length());
+	nocup = line.substr(0, line.find(delimiter));
+
+	nucle=stoi(nucl);
+	nucleusOcup=stoi(nocup);
+
+	while(!f.eof())
+	{
+		if(nucleus<=(nucle - nucleusOcup))
+		{
+			n.push_back(m_id);
+		}
+	}
+	f.close();
+	return n;
 }
 
 bool Maquina::deleteReserva(int machine_ID, int reserva_ID)
@@ -146,14 +193,14 @@ bool Maquina::deleteReserva(int machine_ID, int reserva_ID)
 	ifstream f(machine_ID+".txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
+		cout<<"Error al abrir el fichero"<<endl;
 		EXIT_FAILURE;
 	}
 
 	ofstream f1("f1.txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
+		cout<<"Error al abrir el fichero"<<endl;
 		EXIT_FAILURE;
 	}
 
@@ -170,16 +217,16 @@ bool Maquina::deleteReserva(int machine_ID, int reserva_ID)
 	f.close();
 	f1.close();
 
-	string fnew = maquina+".txt";
+	string fnew = machine_ID+".txt";
 
 	if(remove(fnew.c_str())!=0)
 	{
-		cout<<"Error al eliminar el fichero\n"<<endl;
+		cout<<"Error al eliminar el fichero"<<endl;
 		return false;
 	}
 	else
 	{
-		cout<<"El fichero se ha eliminado\n"<<endl;
+		cout<<"El fichero se ha eliminado"<<endl;
 	}
 
 	rename("f1.txt", (fnew.c_str()));
@@ -187,15 +234,16 @@ bool Maquina::deleteReserva(int machine_ID, int reserva_ID)
 
 string Maquina::modificarMachine(int machine_ID)
 {
-	ifstream f(machine_ID+".txt");
+	ifstream f("Maquinas.txt");
 	if(!f)
 	{
-		cout<<"Error al abrir el fichero\n"<<endl;
+		cout<<"Error al abrir el fichero"<<endl;
 		EXIT_FAILURE;
 	}
 
 	int election;
 	string maquina;
+	getline(f, maquina, ' ');
 	while(!f.eof())
 	{
 		if(maquina == to_string(machine_ID))
@@ -203,24 +251,26 @@ string Maquina::modificarMachine(int machine_ID)
 		system("clear");
 
 		sel:
-		cout<<"1. Eliminar maquina.\n"<<endl;
-		cout<<"2. Modificar maquina.\n"<<endl;
-		cout<<"Seleccione una accion:\n"<<endl;
+		cout<<"Seleccione una accion:"<<endl;
+		cout<<"1. Eliminar maquina."<<endl;
+		cout<<"2. Modificar maquina."<<endl;
 		cin>>election;
 
 		if(election == 1)
 		{
-			return "Eliminar";
+			return "ELIMINAR";
 		}
-		else if(election == 1)
+		else if(election == 2)
 		{
-			return "Modificar";
+			return "MODIFICAR";
 		}
 		else
 		{
 			cout<<"Debe seleccionar o 1 o 2"<<endl;
 			goto sel;
 		}
+		}
 	}
 	f.close();
+	return "CREAR";
 }
